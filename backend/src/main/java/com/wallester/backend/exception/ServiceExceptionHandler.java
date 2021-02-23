c45bb6de57c63bd888301d320515592f3c19e63f
abvgdeej <aleksandr.pitasov@yandexr.ru>
2021-02-23 15:38:55 +0300

    log and exception handler fixes

--- backend/src/main/java/com/wallester/backend/exception/ServiceExceptionHandler.java
+++ backend/src/main/java/com/wallester/backend/exception/ServiceExceptionHandler.java
@@ -2,9 +2,13 @@ package com.wallester.backend.exception;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
@@ -17,22 +21,40 @@ import java.time.OffsetDateTime;
 @ControllerAdvice
 @Slf4j
 public class ServiceExceptionHandler {
+    @ExceptionHandler(value = {Exception.class})
+    @ResponseBody
+    public ResponseErrorDto handleUnknownExceptions(Exception ex) throws Exception {
+        Class<?> exClass = ex.getClass();
+        if (exClass == ApiException.class
+                || exClass == ConstraintViolationException.class
+                || JsonProcessingException.class.isAssignableFrom(exClass)
+                || exClass == IllegalArgumentException.class) {
+            throw ex;
+        }
+        log.error("Unknown error: ", ex);
+
+        ResponseErrorDto dto = new ResponseErrorDto();
+        dto.setTime(OffsetDateTime.now());
+        dto.setMessage(ex.getMessage());
+        dto.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        return dto;
+    }
+
     /**
      * Catches {@link ConstraintViolationException}'s
      *
      * @param ex        Exception
-     * @param response  Server response
      * @return          Response-error DTO
      */
-    @ExceptionHandler(ConstraintViolationException.class)
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public ResponseErrorDto handleValidationExceptions(ConstraintViolationException ex, HttpServletResponse response) {
+    public ResponseErrorDto handleValidationExceptions(ConstraintViolationException ex) {
         log.error("Validation error: ", ex);
         ResponseErrorDto dto = new ResponseErrorDto();
         dto.setTime(OffsetDateTime.now());
         dto.setMessage(ex.getMessage());
         dto.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         return dto;
     }
 
@@ -40,18 +62,17 @@ public class ServiceExceptionHandler {
      * Catches Jackson's exception after JSON parsing.
      *
      * @param ex        Exception
-     * @param response  Server response
      * @return          Response-error DTO
      */
-    @ExceptionHandler(value = JsonProcessingException.class)
+    @ExceptionHandler(value = {JsonProcessingException.class, HttpMessageNotReadableException.class, HttpMessageNotWritableException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public ResponseErrorDto handleJacksonError(JsonProcessingException ex, HttpServletResponse response) {
+    public ResponseErrorDto handleJacksonError(JsonProcessingException ex) {
         log.error("Jackson error: ", ex);
         ResponseErrorDto dto = new ResponseErrorDto();
         dto.setTime(OffsetDateTime.now());
         dto.setMessage("JSON parse exception, check your request params");
         dto.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         return dto;
     }
 
@@ -62,22 +83,22 @@ public class ServiceExceptionHandler {
      * @param response  Server response
      * @return          Response-error DTO
      */
-    @ExceptionHandler(value = ApiException.class)
+    @ExceptionHandler(value = {ApiException.class})
     @ResponseBody
     public ResponseErrorDto handleApiExceptions(ApiException ex, HttpServletResponse response) {
         log.error("API exception: ", ex);
         return getResponseErrorDto(ex, response);
     }
 
-    @ExceptionHandler(value = IllegalArgumentException.class)
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public ResponseErrorDto handleIllegalArgumentExceptions(IllegalArgumentException ex, HttpServletResponse response) {
+    public ResponseErrorDto handleIllegalArgumentExceptions(IllegalArgumentException ex) {
         log.error("IllegalArgumentException exception: {}", ex.getMessage(), ex);
         ResponseErrorDto dto = new ResponseErrorDto();
         dto.setTime(OffsetDateTime.now());
         dto.setMessage(ex.getMessage());
         dto.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         return dto;
     }
 
